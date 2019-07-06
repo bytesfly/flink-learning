136a62af7e10c6a2b544afff61f1668f03a74d4e
firstsword <firstsword@foxmail.com>
2019-07-06 10:52:06 +0800

    init proj

---
+++ src/main/java/com/github/firstsword/flink/learning/java/examples/BatchJob.java
@@ -0,0 +1,52 @@
+package com.github.firstsword.flink.learning.java.examples;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+
+import java.util.Arrays;
+
+/**
+ * Skeleton for a Flink Batch Job.
+ *
+ * <p>For a tutorial how to write a Flink batch application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution,
+ * change the main class in the POM.xml file to this class (simply search for 'mainClass')
+ * and run 'mvn clean package' on the command line.
+ */
+public class BatchJob {
+
+    public static void main(String[] args) throws Exception {
+        // set up the batch execution environment
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        /*
+         * Here, you can start creating your execution plan for Flink.
+         *
+         * Start with getting some data from the environment, like
+         * 	env.readTextFile(textPath);
+         *
+         * then, transform the resulting DataSet<String> using operations
+         * like
+         * 	.filter()
+         * 	.flatMap()
+         * 	.join()
+         * 	.coGroup()
+         *
+         * and many more.
+         * Have a look at the programming guide for the Java API:
+         *
+         * http://flink.apache.org/docs/latest/apis/batch/index.html
+         *
+         * and the examples
+         *
+         * http://flink.apache.org/docs/latest/apis/batch/examples.html
+         *
+         */
+
+        // execute program
+        DataSource<Integer> data = env.fromCollection(Arrays.asList(1, 2, 3));
+        data.map(x -> x+1).print();
+        //env.execute("Flink Batch Java API Skeleton");
+    }
+}
---
+++ src/main/scala/com/github/firstsword/flink/learning/scala/ScalaApp.scala
@@ -0,0 +1,7 @@
+package com.github.firstsword.flink.learning.scala
+
+object ScalaApp {
+  def main(args: Array[String]): Unit = {
+    println("hello, flink!")
+  }
+}
